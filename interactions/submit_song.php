49d0ed60b782b3650622945d468e6a2ee3726a63
sophiea317 <88386142+sophiea317@users.noreply.github.com>
2023-04-30 01:19:13 +0000

    update style.css

--- interactions/submit_song.php
+++ interactions/submit_song.php
@@ -7,14 +7,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
   // Validate the input
   if(empty($song)){
-    echo "Please enter a song recommendation";
+    echo "please enter a song recommendation";
   } else {
     // Store the song recommendation in a text file
     $file = fopen("song_recommendations.txt", "a") or die("Unable to open file!");
     fwrite($file, $song . "\n");
     fclose($file);
-    
-    echo "Thank you for your song recommendation!";
   }
 }
 
